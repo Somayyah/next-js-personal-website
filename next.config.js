e496d7f8d3f10e4f7f1b72eecf27a31b4c1d07cd
somayyah <smalfraihat131@cit.just.edu.jo>
2020-12-24 17:29:41 +0200

    Done

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 //require("dotenv").config({ path: `${process.env.ENVIRONMENT}` });
+const withMDX = require('@next/mdx'){};
 
 module.exports = {
 	distDir: "build",
 	webpack: (config, { isServer }) => {
     		if (isServer) {
-			//require('./pages/sitemap.xml.js');
       			require('./scripts/generate-sitemap');
     		}
 
     	return config;
-  	}
+  	},
+	withMDX({
+		pageExtensions: ['js', 'mdx']
+	})
 };
